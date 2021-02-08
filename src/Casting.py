96739e66096ef69597f6fbbc1e9b68e443edf453
Michael Friderich <michael@frideri.ch>
2021-02-08 21:21:46 +0100

    add Casting.py

---
+++ src/Casting.py
@@ -0,0 +1,11 @@
+x = int(7)
+print(x)        #Output 7
+print(type(x))  #Output <class 'int'>
+
+x = float(7)
+print(x)        #Output 7.0
+print(type(x))  #Output <class 'float'>
+
+x = str(7)
+print(x)        #Output 7
+print(type(x))  #Output <class 'str'>
