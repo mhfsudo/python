707df188dc5275b01f0d40a24320c65609670f7c
Michael Friderich <michael@frideri.ch>
2021-02-08 21:22:21 +0100

    add Variable.py

---
+++ src/Variable.py
@@ -0,0 +1,19 @@
+x = 7
+print(x)    #output 7
+
+x = "Alice"
+print(x)    #output Alice
+
+#assign value to multiple variables
+x, y = 7, "Alice"
+print(x,y)  #output 7 Alice
+
+#add variables
+a = 3
+b = 7
+c = "Hello"
+d = "World"
+
+print(a + b)    #output 10
+print(c + d)    #output HelloWorld
+#print(a + c)   #output error
