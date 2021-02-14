121c1209c53d39e5500099533f9af7519a580193
Michael Friderich <michael@frideri.ch>
2021-02-14 09:22:13 +0100

    add Dictionary.py

--- src/Dictionary.py
+++ src/Dictionary.py
@@ -1,40 +1,43 @@
-#In Java map
+# In Java map
 empty = {}
 print(empty)
 
-food = {"Salad" : "yes", "Steak" : "no", "Cake" : "yes"}
+food = {"Salad": "yes", "Steak": "no", "Cake": "yes"}
 print(food)
 
 food["Steak"] = "yes"
 print(food)
 
-#Verschachteltes Dictionary
-deEn = {"rot" : "red", "blau" : "blue", "grün" : "green"}
-enFr = {"red" : "rouge", "blue" : "bleu", "green" : "vert"}
+# Dictionary länge
+print(len(food))    # output 3
+
+# Verschachteltes Dictionary
+deEn = {"rot": "red", "blau": "blue", "grün": "green"}
+enFr = {"red": "rouge", "blue": "bleu", "green": "vert"}
 
 print(deEn["rot"])
 print(enFr[deEn["rot"]])
 
-#Dictionary von Dictionaries
-enDe = {"red":"rot", "blue":"blau", "green":"grün"}
-deFr = {"rot":"rouge", "blau":"bleu", "grün":"vert"}
+# Dictionary von Dictionaries
+enDe = {"red": "rot", "blue": "blau", "green": "grün"}
+deFr = {"rot": "rouge", "blau": "bleu", "grün": "vert"}
 
-dictionaries = {"enDe" : enDe, "deFr" : deFr }
+dictionaries = {"enDe": enDe, "deFr": deFr}
 print(dictionaries["deFr"]["blau"])
 
-#Dictionary verändern
-capitals = {"Schweiz":"Bern", "Östereich":"Wien"}
+# Dictionary verändern
+capitals = {"Schweiz": "Bern", "Östereich": "Wien"}
 capital = capitals.pop("Östereich")
 print(capital)
 print(capitals)
 
-capital = capitals.pop("Deutschland","Unbekannt")
+capital = capitals.pop("Deutschland", "Unbekannt")
 print(capital)
 
 capital = capitals.get("Schweiz")
-print(capital)                  #Gibt Bern aus
+print(capital)  # Gibt Bern aus
 
-#Dictionary in Liste/Set umwandel
+# Dictionary in Liste/Set umwandel
 dD = {"Eins", "Zwei"}
 print(dD)
 
