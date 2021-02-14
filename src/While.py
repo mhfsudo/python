dfa429c24944763bbc46aa8abd9e0f8b6851ff5b
Michael Friderich <michael@frideri.ch>
2021-02-14 09:22:47 +0100

    add While.py

--- src/While.py
+++ src/While.py
@@ -13,3 +13,19 @@ while i >= 0 :
     i -= 1
 else:
     print("Programm finished")
+
+# Break
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+# Continue
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
